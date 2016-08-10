818befe5115c985229a2052ad2f442b825d558ba
Daniel Barranco <danielbarranco@gmail.com>
2016-08-09 22:25:42 -0400

    updates to next page button

--- movieAngular.js
+++ movieAngular.js
@@ -3,6 +3,7 @@ movieApp.controller('movieController', function($scope, $http){
 
 	var npMovieURL = 'https://api.themoviedb.org/3/movie/now_playing?api_key=fec8b5ab27b292a68294261bb21b04a5';
 	var movieURL = 'https://api.themoviedb.org/3/search/movie?api_key=fec8b5ab27b292a68294261bb21b04a5&query=';
+	var genreURL = 'https://api.themoviedb.org/3/genre/movie/list?api_key=fec8b5ab27b292a68294261bb21b04a5';
 	$scope.imagePath = 'http://image.tmdb.org/t/p/w300/';
 
 	$scope.myStyle = "orange";
@@ -12,6 +13,7 @@ movieApp.controller('movieController', function($scope, $http){
 		url: npMovieURL
 		}).then(function successFunction(searchData){
 			$scope.movieArray = searchData.data.results;
+			console.log(genreURL);
 		},function failureFunction(searchData){
 			console.log(movieData);
 		}
@@ -23,8 +25,21 @@ movieApp.controller('movieController', function($scope, $http){
 		url: movieURL + $scope.queryString
 		}).then(function successFunction(searchData){
 			$scope.movieArray = searchData.data.results;
+			console.log(searchData);
 		},function failureFunction(searchData){
-			console.log(movieData);
+			
+		}
+	);
+	}
+
+	$scope.nextPage = function(){
+		$http({
+		method: 'GET',
+		url: movieURL + $scope.queryString + '&page=2'
+		}).then(function successFunction(searchData){
+			$scope.movieArray = searchData.data.results;
+		},function failureFunction(searchData){
+			
 		}
 	);
 	}
